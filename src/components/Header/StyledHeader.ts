10ba64ff04c0beaad64b184bd9a791a65d7ab317
Fearinn <matheusmgomes09@gmail.com>
2023-02-27 22:07:18 -0300

    feat Header component added

---
+++ src/components/Header/StyledHeader.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+display: flex;
+align-items: center;
+justify-content: space-between;
+background-color: rgb(255, 64, 129);
+color: white;
+padding: 1rem;
+`
+export default StyledHeader
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import Search from "../Search";
+import StyledHeader from "./StyledHeader";
+
+export function Header() {
+  return (
+    <StyledHeader>
+      <nav>
+        <h1>
+          <Link to="/">Wolvesville Wiki</Link>
+        </h1>
+      </nav>
+      <Search />
+    </StyledHeader>
+  );
+}
