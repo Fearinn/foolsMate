8e52f551f3329a43bf54b7c1bc52d8d49d8b68b2
Fearinn <matheusmgomes09@gmail.com>
2023-02-27 21:46:29 -0300

    initial: project initialized with pnpm

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import DefaultPage from "./pages/DefaultPage";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<DefaultPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
