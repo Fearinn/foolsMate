7f9ec6f387c259bf9259b51a8a8c6e99506bfb9f
Fearinn <matheusmgomes09@gmail.com>
2023-02-27 21:50:35 -0300

    refactor: App wrapped into ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "@total-typescript/ts-reset";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
