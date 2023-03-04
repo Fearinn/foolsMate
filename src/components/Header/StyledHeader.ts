ecb263a0833f5c70b648079e53f083502f05e08d
Fearinn <matheusmgomes09@gmail.com>
2023-03-04 17:44:04 -0300

    css: simple styles added in Header nav

--- src/components/Header/StyledHeader.ts
+++ src/components/Header/StyledHeader.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components"
+import { colors } from "../../assets/cssVariables"
 
 const StyledHeader = styled.header`
 display: flex;
 align-items: center;
 justify-content: space-between;
-background-color: rgb(255, 64, 129);
-color: white;
+background-color: ${colors.mainBrand};
+color: ${colors.secondaryFont};
 padding: 1rem;
+
+nav {
+    display: flex;
+    gap: 1rem;
+    padding: 0 0.5rem;
+    font-weight: bold;
+}
 `
 export default StyledHeader
