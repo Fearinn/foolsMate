f22abdfccdfb476b67dc433e58ab22189fe3671c
Fearinn <matheusmgomes09@gmail.com>
2023-03-03 21:17:41 -0300

    refactor: function 'getAllItems' turned into more specific 'getAvarItems'

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,15 @@
 import axios from "axios";
+import IAvatarItem from "../types/AvatarItem";
+import IResponseData from "../types/ResponseData";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000/",
 });
 
-async function getAllItems<T = unknown>(path: string) {
-  const response = await instance.get<T[]>(`items/${path}`);
+async function getAvatarItems() {
+  const response = await instance.get<IResponseData<IAvatarItem>>(`items/avatarItems`);
   console.log(response.data)
   return response.data;
 }
 
-export { getAllItems };
+export { getAvatarItems };
