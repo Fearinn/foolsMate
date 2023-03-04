6fb5f25485f394f749f3ebd64637ae268b2c815b
Fearinn <matheusmgomes09@gmail.com>
2023-03-04 17:32:08 -0300

    css: colors variables defined

---
+++ src/assets/cssVariables.ts
@@ -0,0 +1,14 @@
+export const colors = {
+    mainBrand: "rgb(255, 64, 129)",
+    mainFont: "#000",
+    secondaryFont: "#FFF",
+    costInGold: "#FF884B",
+    maleGender: "#2D46B9",
+    femaleGender: "#F037A5",
+    neutralGender: "#21AA93",
+    commonRarity: "#6A492B",
+    rareRarity: "#A13333",
+    epicRarity: "#A31ACB",
+    legendaryRarity: "#E27802",
+    itemType: "#4A772F"
+}
