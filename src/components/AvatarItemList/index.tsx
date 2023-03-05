5b108b62cbd5102110fa88e9ed9c0c6d13610ade
Fearinn <matheusmgomes09@gmail.com>
2023-03-04 22:54:33 -0300

    refactor: Spinner and error message added in AvatarItemsList

--- src/components/AvatarItemList/index.tsx
+++ src/components/AvatarItemList/index.tsx
@@ -2,14 +2,35 @@ import { useQuery } from "react-query";
 import { getAvatarItems } from "../../services";
 import AvatarItemCard from "./AvarItemCard";
 import StyledAvatarItemList from "./StyledAvatarItemList";
+import { Spinner } from "@chakra-ui/react";
+import { AxiosError } from "axios";
+import IAvatarItem from "../../types/AvatarItem";
+import IResponseData from "../../types/ResponseData";
 
 function AvatarItemsList() {
-  const { data, error } = useQuery(["getAvatarItems"], getAvatarItems, {
+  const { data, isLoading, error } = useQuery<
+    IResponseData<IAvatarItem>,
+    AxiosError
+  >(["getAvatarItems"], getAvatarItems, {
     staleTime: 1000 * 60 * 30,
-    keepPreviousData: true
+    keepPreviousData: true,
   });
 
-  if (!data || error) return <></>;
+  if (isLoading)
+    return (
+      <>
+        <Spinner size={"xl"} marginBottom={"1rem"} />
+        <p>Data is being fetched...</p>
+      </>
+    );
+
+  if (!data || error)
+    return (
+      <p role="alert">
+        Sorry, an unexpected error has ocurred! Try again later. [Error:
+        {error?.message || "unknown error"}]
+      </p>
+    );
 
   return (
     <StyledAvatarItemList>
