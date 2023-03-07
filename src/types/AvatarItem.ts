e031181953c70928ff4eab3ddc7bc7c30a8657f5
Fearinn <matheusmgomes09@gmail.com>
2023-03-07 14:54:12 -0300

    refactor: missing props in type AvatarItem added

--- src/types/AvatarItem.ts
+++ src/types/AvatarItem.ts
@@ -1,10 +1,29 @@
 type IAvatarItem = {
   id: string;
-  rarity: "COMMON" | "RARE" | "EPIC" | "LEGENDARY";
-  costInGold: number;
+  rarity: IAvatarRarity;
+  costInGold?: number;
+  costInRoses?: number;
   imageUrl: "string";
-  type: "HAIR" | "FRONT" | "SHIRT" | "HAT" | "GLASSES" | "BACK" | "MASK" | "GRAVESTONE" | "MOUTH",
-  gender: "FEMALE" | "MALE"
+  type: IAvatarItemKind;
+  gender?: IAvatarItemGender;
+  event?: string;
 };
+
+type IAvatarRarity = "COMMON" | "RARE" | "EPIC" | "LEGENDARY";
+
+type IAvatarItemKind =
+  | "HAIR"
+  | "FRONT"
+  | "SHIRT"
+  | "HAT"
+  | "GLASSES"
+  | "BACK"
+  | "MASK"
+  | "GRAVESTONE"
+  | "MOUTH";
+
+type IAvatarItemGender = "FEMALE" | "MALE" | ""
+
+export type { IAvatarItemKind, IAvatarItemGender, IAvatarRarity};
 
 export default IAvatarItem;
