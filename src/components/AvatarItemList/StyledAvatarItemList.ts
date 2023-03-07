a15f6559904bdb527fba29ad4daa22df379fccae
Fearinn <matheusmgomes09@gmail.com>
2023-03-07 19:12:32 -0300

    feat: filters and pagination added to AvatarItemList

--- src/components/AvatarItemList/StyledAvatarItemList.ts
+++ src/components/AvatarItemList/StyledAvatarItemList.ts
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 
-const StyledAvatarItemList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  align-items: stretch;
-  gap: 0.5rem;
-  background-color: #EFEFEF;
-  font-size: 1.125rem;
-  padding: 1rem;
+const StyledAvatarItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
   width: 100%;
 
-  li {
+  .filters {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    gap: 0.5rem;
+  }
+
+  ul {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    align-items: stretch;
+    gap: 0.5rem;
+    background-color: #efefef;
+    font-size: 1.125rem;
+    padding: 1rem;
     width: 100%;
+
+    li {
+      width: 100%;
+    }
   }
 `;
 
--- src/components/AvatarItemList/index.tsx
+++ src/components/AvatarItemList/index.tsx
@@ -2,19 +2,63 @@ import { useQuery } from "react-query";
 import { getAvatarItems } from "../../services";
 import AvatarItemCard from "./AvarItemCard";
 import StyledAvatarItemList from "./StyledAvatarItemList";
-import { Spinner } from "@chakra-ui/react";
+import { Button, Input, Select, Spinner } from "@chakra-ui/react";
 import { AxiosError } from "axios";
-import IAvatarItem from "../../types/AvatarItem";
+import IAvatarItem, {
+  IAvatarItemGender,
+  IAvatarItemKind,
+  IAvatarRarity,
+} from "../../types/AvatarItem";
 import IResponseData from "../../types/ResponseData";
+import { useEffect, useState } from "react";
+import { colors } from "../../assets/cssVariables";
 
 function AvatarItemsList() {
-  const { data, isLoading, error } = useQuery<
+  const [limit, setLimit] = useState(100);
+  const [page, setPage] = useState(1);
+  const [gender, setGender] = useState<string>();
+  const [rarity, setRarity] = useState<string>();
+  const [type, setType] = useState<string>();
+  const [event, setEvent] = useState<string>();
+
+  function handleGender(gender: string | undefined) {
+    if (gender === "BOTH") return "";
+    if (gender === "") return undefined;
+    return gender as IAvatarItemGender;
+  }
+
+  const [filters, setFilters] = useState<Partial<IAvatarItem>>({
+    gender: handleGender(gender),
+    rarity: (rarity as IAvatarRarity) || undefined,
+    type: (type as IAvatarItemKind) || undefined,
+    event: event || undefined,
+  });
+
+  function pageOptions() {
+    const numberList: number[] = [];
+    for (let i = 1; i <= (numberOfPages || 0); i++) {
+      numberList.push(i);
+    }
+    return numberList;
+  }
+
+  const { data, isLoading, error, refetch } = useQuery<
     IResponseData<IAvatarItem>,
     AxiosError
-  >(["getAvatarItems"], getAvatarItems, {
+  >(["getAvatarItems", filters], () => getAvatarItems(limit, page, filters), {
     staleTime: 1000 * 60 * 30,
     keepPreviousData: true,
   });
+
+  const [numberOfPages, setNumberOfPages] = useState(data?.numberOfPages);
+
+  useEffect(() => {
+    if (!isLoading) refetch();
+  }, [filters]);
+
+  useEffect(() => {
+    setNumberOfPages(Math.ceil(Number(data?.totalCount) / limit) || 1);
+  }, [limit]);
 
   if (isLoading)
     return (
@@ -24,23 +68,122 @@ function AvatarItemsList() {
       </>
     );
 
-  if (!data || error)
+  if (!data || error) {
     return (
       <p role="alert">
         Sorry, an unexpected error has ocurred! Try again later. [Error:
         {error?.message || "unknown error"}]
       </p>
     );
+  }
 
   return (
     <StyledAvatarItemList>
-      {data.items.map((item) => {
-        return (
-          <li key={item.id}>
-            <AvatarItemCard {...item} />
-          </li>
-        );
-      })}
+      <div className="filters">
+        <Select
+          placeholder="Items Per Page"
+          width="auto"
+          variant="filled"
+          onChange={(event) => {
+            setLimit(Number(event.target.value) || 100);
+          }}
+        >
+          <option value={100}>100 (default)</option>
+          <option value={500}>500</option>
+          <option value={1000}>1000</option>
+        </Select>
+        <Select
+          placeholder="Gender"
+          width="auto"
+          variant="filled"
+          onChange={(event) => {
+            setGender(event.target.value);
+          }}
+        >
+          <option value={"BOTH"}>Both</option>
+          <option value={"MALE"}>Male</option>
+          <option value={"FEMALE"}>Female</option>
+        </Select>
+        <Select
+          placeholder="Rarity"
+          width="auto"
+          variant="filled"
+          onChange={(event) => {
+            setRarity(event.target.value);
+          }}
+        >
+          <option value={"COMMON"}>Common</option>
+          <option value={"RARE"}>Rare</option>
+          <option value={"EPIC"}>Epic</option>
+          <option value={"LEGENDARY"}>Legendary</option>
+        </Select>
+        <Select
+          placeholder="Type"
+          width="auto"
+          variant="filled"
+          onChange={(event) => {
+            setType(event.target.value);
+          }}
+        >
+          <option value={"HAIR"}>Hair</option>
+          <option value={"FRONT"}>Front</option>
+          <option value={"SHIRT"}>Shirt</option>
+          <option value={"HAT"}>Hat</option>
+          <option value={"GLASSES"}>Glasses</option>
+          <option value={"MOUTH"}>Mouth</option>
+          <option value={"GRAVESTONE"}>Gravestone</option>
+          <option value={"BACK"}>Back</option>
+          <option value={"MASK"}>Mask</option>
+        </Select>
+        <Select
+          placeholder="Page"
+          width="auto"
+          variant="filled"
+          onChange={(event) => {
+            setPage(Number(event.target.value) || 1);
+          }}
+        >
+          {pageOptions().map((number) => {
+            return (
+              <option key={number} value={number}>
+                {number}
+              </option>
+            );
+          })}
+        </Select>
+        <Input
+          width="auto"
+          size="md"
+          type="text"
+          variant="filled"
+          placeholder="Event"
+          onBlur={(event) => setEvent(event.target.value)}
+        />
+        <Button
+          backgroundColor={colors.mainBrand}
+          _hover={{ background: colors.mainBrand, opacity: 0.8 }}
+          color={colors.secondaryFont}
+          onClick={() => {
+            setFilters({
+              gender: handleGender(gender),
+              rarity: (rarity as IAvatarRarity) || undefined,
+              type: (type as IAvatarItemKind) || undefined,
+              event: event || undefined,
+            });
+          }}
+        >
+          Filter
+        </Button>
+      </div>
+      <ul>
+        {data.items.map((item) => {
+          return (
+            <li key={item.id}>
+              <AvatarItemCard {...item} />
+            </li>
+          );
+        })}
+      </ul>
     </StyledAvatarItemList>
   );
 }
