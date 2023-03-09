ed6bb28801c76124184e91928ee771088d065615
Fearinn <matheusmgomes09@gmail.com>
2023-03-09 16:43:44 -0300

    feat: Footer added

--- src/pages/DefaultPage/index.tsx
+++ src/pages/DefaultPage/index.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import Footer from "../../components/Footer";
 import { Header } from "../../components/Header";
 
 function DefaultPage() {
@@ -6,6 +7,7 @@ function DefaultPage() {
     <>
       <Header />
       <Outlet />
+      <Footer/>
     </>
   );
 }
