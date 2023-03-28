1380c97a61e428cad6c1cc90d3d655aa78ed01c6
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-03-28 15:31:22 -0300

    css(Filters): min length of grid-items reduced

--- src/components/Filters/StyledFilters.ts
+++ src/components/Filters/StyledFilters.ts
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const StyledFilters = styled.form`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   gap: 0.5rem;
 `;
