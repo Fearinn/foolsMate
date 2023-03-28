98bf9b5f38a4d728d58037cc8e9dc0929c746de4
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-03-27 23:54:43 -0300

    refactor(Header): title moved out of nav

--- src/components/Header/StyledHeader.ts
+++ src/components/Header/StyledHeader.ts
@@ -4,16 +4,31 @@ import { colors } from "../../assets/cssVariables";
 const StyledHeader = styled.header`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  gap: 2rem;
   background-color: ${colors.mainBrand};
   color: ${colors.secondaryFont};
+  font-weight: bold;
   padding: 1rem;
+
+  h2 {
+    display: flex;
+    gap: 0.5rem;
+    color: ${colors.secondaryFont};
+    border-radius: 4px;
+    font-size: 1.5rem;
+    padding: 0 0.5rem;
+  }
+
+  img {
+    aspect-ratio: 1;
+  }
 
   nav {
     display: flex;
+    align-items: center;
     gap: 1rem;
     padding: 0 0.5rem;
-    font-weight: bold;
+    font-size: 1.25rem;
   }
 `;
 export { StyledHeader };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,10 +4,18 @@ import { StyledHeader } from "./StyledHeader";
 function Header() {
   return (
     <StyledHeader>
+      <Link to="/">
+        <h2>
+          <img
+            src="/paw-3-svgrepo-com.svg"
+            role="presentation"
+            width="24"
+            height="24"
+          ></img>
+          Wolvesville Wiki
+        </h2>
+      </Link>
       <nav>
-        <h1>
-          <Link to="/">Wolvesville Wiki</Link>
-        </h1>
         <Link to="items/avatarItems">Items</Link>
       </nav>
     </StyledHeader>
