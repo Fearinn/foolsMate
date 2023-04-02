cefe09b810acef83cbdaa8f2794fe6beef80ed63
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-04-02 10:11:55 -0300

    feat(Home): add BattlePass component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,34 @@
 import { Heading } from "@chakra-ui/react";
+import { BattlePass } from "../../components/BattlePass";
+import { ErrorMessage } from "../../components/ErrorMessage";
+import { Loader } from "../../components/Loader";
 import { MainTitle } from "../../components/MainTitle";
+import { useBattlePassSeason } from "../../utils/hooks/useBattlePassSeason";
+import { StyledHome } from "./StyledHome";
 
 function Home() {
+  const { data, isLoading, error } = useBattlePassSeason(["AVATAR_ITEM"]);
+
+  if (isLoading)
+    return (
+      <StyledHome>
+        <Loader />
+      </StyledHome>
+    );
+
+  if (!data || error) {
+    return (
+      <StyledHome>
+        <ErrorMessage />
+      </StyledHome>
+    );
+  }
   return (
-    <main>
+    <StyledHome>
       <MainTitle title="Welcome to Wolvesville Wiki!" />
       <Heading size="md">{"What's new in Wolvesville?"}</Heading>
-    </main>
+      <BattlePass {...data}></BattlePass>
+    </StyledHome>
   );
 }
 
