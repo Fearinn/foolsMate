b9e8d59970c86801a6c2d46a8030e9af9ce77f1d
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-05-06 18:58:51 -0300

    feat(Home): prefetch query with SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,30 @@ import { BattlePass } from "@/components/BattlePass";
 import { ErrorMessage } from "@/components/ErrorMessage";
 import { Loader } from "@/components/Loader";
 import { MainTitle } from "@/components/MainTitle";
+import { getBattlePassSeason } from "@/services";
 import { useBattlePassSeason } from "@/utils/hooks/useBattlePassSeason";
 import { Heading } from "@chakra-ui/react";
 import Head from "next/head";
+import { dehydrate, QueryClient } from "react-query";
 import { StyledHome } from "./StyledHome";
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    ["getBattlePassSeason"],
+    () => getBattlePassSeason(["AVATAR_ITEM"]),
+    {
+      staleTime: 1000 * 60 * 30,
+    }
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 
 export default function Home() {
   const { data, isLoading, error } = useBattlePassSeason(["AVATAR_ITEM"]);
@@ -28,9 +48,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Wolvesville Wiki</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Wolvesville Wiki</title>s
       </Head>
       <StyledHome>
         <MainTitle title="Welcome to Wolvesville Wiki!" />
