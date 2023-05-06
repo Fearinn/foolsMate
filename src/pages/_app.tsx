adbee2fb406707ed4500d9a64b619bdeadd91210
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-05-06 18:56:56 -0300

    fix(App): add Head with viewport and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useState } from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 import Layout from "../components/Layout";
@@ -9,14 +10,23 @@ export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <ChakraProvider>
-          <Layout> 
-            <Component {...pageProps} />
-          </Layout>
-        </ChakraProvider>
-      </Hydrate>
-    </QueryClientProvider>
+    <>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <ChakraProvider>
+            <Layout>
+              <Head>
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.svg" />
+              </Head>
+              <Component {...pageProps} />
+            </Layout>
+          </ChakraProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </>
   );
 }
