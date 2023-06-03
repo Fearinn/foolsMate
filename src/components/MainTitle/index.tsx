f7b07969fd1d6f53928ed9b8c70e5964686d8057
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-06-03 09:53:57 -0300

    css(MainTitle): align text to center

--- src/components/MainTitle/index.tsx
+++ src/components/MainTitle/index.tsx
@@ -1,8 +1,12 @@
 import { ReactNode } from "react";
 import { Heading } from "@chakra-ui/react";
 
-function MainTitle({ title }: { title: string | ReactNode }) {
-  return <Heading as="h1">{title}</Heading>;
+function MainTitle({ title }: { title: ReactNode }) {
+  return (
+    <Heading as="h1" textAlign={"center"}>
+      {title}
+    </Heading>
+  );
 }
 
 export { MainTitle };
