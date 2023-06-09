ffe8a814a74418617aa29321eceacf711225c47c
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-06-09 14:25:01 -0300

    refactor(hooks/battlePass): add 'useRewards'

--- src/utils/hooks/battlePass.ts
+++ src/utils/hooks/battlePass.ts
@@ -1,7 +1,11 @@
-import { RewardType, Season } from "@/components/BattlePass/battlePass.types";
+import {
+  Reward,
+  RewardType,
+  Season,
+} from "@/components/BattlePass/battlePass.types";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import { getBattlePassSeason } from "../../services";
+import { getBattlePassSeason, getRewards } from "../../services";
 
 export function useBattlePassSeason(rewardsTypes: RewardType[]) {
   const response = useQuery<Season, AxiosError>(
@@ -12,6 +16,17 @@ export function useBattlePassSeason(rewardsTypes: RewardType[]) {
       keepPreviousData: true,
     }
   );
+
+  return response;
+}
+
+export function useRewards() {
+  const response = useQuery<Reward[], AxiosError>({
+    queryKey: ["getRewards"],
+    queryFn: getRewards,
+    staleTime: 1000 * 60 * 30,
+    keepPreviousData: true,
+  });
 
   return response;
 }
