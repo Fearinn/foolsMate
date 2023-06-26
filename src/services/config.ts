3660f5c5a614d30acebdd1462cc4d283c2cc4d4e
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-06-26 13:00:25 -0300

    chore(services/config): set baseURL based on environment

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const prod = process.env.NODE_ENV === "production";
+
 export const instance = axios.create({
-  baseURL: "https://foolsmate.cyclic.app/",
+  baseURL: prod ? "https://foolsmate.cyclic.app/" : "http://localhost:3000",
   headers: {
     Accept: "application/json",
   },
