f1d014142313c5f8bf474c36d4c17f7d8918f5d5
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-06-26 13:01:27 -0300

    chore(next.config): add CSP and security headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === "production";
+
+const CSP = `default-src 'self'; font-src 'self' https://fonts.google.com; style-src 'self' 'unsafe-inline'; connect-src 'self' ${
+  prod ? "https://foolsmate.cyclic.app/" : "http://localhost:3000"
+};${!prod ? " script-src 'self' 'unsafe-eval'" : ""}`;
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        locale: false,
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosiff" },
+          { key: "Content-Security-Policy", value: CSP },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
