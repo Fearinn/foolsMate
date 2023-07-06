41279dd0c241f1e230b6960bcdcbb62d10efff6e
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-07-06 16:46:47 -0300

    chore: add og:type with 'website' as content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import {
   Hydrate,
   QueryClient,
-  QueryClientProvider
+  QueryClientProvider,
 } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
@@ -50,7 +50,11 @@ export default function App({ Component, pageProps }: AppProps) {
                   name="description"
                   content="Track Wolvesville battle pass season, items, players stats and more!"
                 />
-                <meta property="og:title" content="Fool's Mate - Wolvesville Online Tracker"></meta>
+                <meta property="og:type" content="website" />
+                <meta
+                  property="og:title"
+                  content="Fool's Mate - Wolvesville Online Tracker"
+                ></meta>
                 <meta
                   property="og:description"
                   content="Track Wolvesville battle pass season, items, players stats and more!"
