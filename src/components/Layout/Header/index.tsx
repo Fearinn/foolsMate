a31cbca6bc326662ccf217350853b85264165c26
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2023-07-28 22:00:50 -0300

    feat(Header): add popup menu to mobile

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/Button";
 import { Heading } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Header.module.scss";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
-function Header() {
+const iconStyles = {
+  size: 24,
+};
+
+export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="/">
@@ -16,21 +25,54 @@ function Header() {
         ></Image>
         <Heading size="lg">{"Fool's Mate"}</Heading>
       </Link>
-      <nav className={styles["main-menu"]}>
-        <Link href="/players">Players</Link>
-        <Link href="/roles">Roles</Link>
-        <Link href="/items/avatarItems">Avatar Items</Link>
-        <Link href="/items/roleIcons">Role Icons</Link>
-        <Link
-          href="https://www.wolvesville.com"
-          target="_blank"
-          className={styles.game}
+      <div className={styles["popup-menu-container"]}>
+        <Button
+          aria-haspopup="menu"
+          aria-controls="main-menu"
+          aria-expanded={isOpen}
+          onClick={() => setIsOpen(!isOpen)}
         >
-          Play
-        </Link>
+          <GiHamburgerMenu {...iconStyles} />
+        </Button>
+        <nav
+          className={styles["main-menu"] + " " + styles["popup-menu"]}
+          id="main-menu"
+        >
+          {isOpen && (
+            <>
+              <Link href="/players">Players</Link>
+              <Link href="/roles">Roles</Link>
+              <Link href="/items/avatarItems">Avatar Items</Link>
+              <Link href="/items/roleIcons">Role Icons</Link>
+              <Link
+                href="https://www.wolvesville.com"
+                target="_blank"
+                className={styles.game}
+              >
+                Play
+              </Link>
+            </>
+          )}
+        </nav>
+      </div>
+      <nav
+        className={styles["main-menu"] + " " + styles["static-menu"]}
+        id="main-menu"
+      >
+        <>
+          <Link href="/players">Players</Link>
+          <Link href="/roles">Roles</Link>
+          <Link href="/items/avatarItems">Avatar Items</Link>
+          <Link href="/items/roleIcons">Role Icons</Link>
+          <Link
+            href="https://www.wolvesville.com"
+            target="_blank"
+            className={styles.game}
+          >
+            Play
+          </Link>
+        </>
       </nav>
     </header>
   );
 }
-
-export { Header };
